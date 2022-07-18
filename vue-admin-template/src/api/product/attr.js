0b1f633a33189690fe27f5641f4614c17ed3eb23
zzh-Sonder <zhangziheng_Sonder@163.com>
2022-07-19 07:31:15 +0800

    属性列表的完善

--- vue-admin-template/src/api/product/attr.js
+++ vue-admin-template/src/api/product/attr.js
@@ -8,11 +8,17 @@ export const reqCategory1List = () => request({
 
 ///admin/product/get/category2/{id} 二级
 export const reqCategory2List = (category1Id) => request({
-    url: `/admin/product/getCategory2/${category1Id}`,
-    method: "get"
-})
-///admin/product/get/category3/{id} 二级
+        url: `/admin/product/getCategory2/${category1Id}`,
+        method: "get"
+    })
+    ///admin/product/get/category3/{id} 三级
 export const reqCategory3List = (category2Id) => request({
     url: `/admin/product/getCategory3/${category2Id}`,
     method: "get"
+})
+
+// /admin/product/attrInfoList/{category1Id}/{category2Id}/{category3Id} 平台属性信息
+export const reqAttrList = (category1Id, category2Id, category3Id) => request({
+    url: `/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`,
+    method: 'get'
 })
--- vue-admin-template/src/views/product/Attr/index.vue
+++ vue-admin-template/src/views/product/Attr/index.vue
@@ -9,9 +9,46 @@
         ></CategorySelect>
       </div>
     </el-card>
+
     <el-card>
-      <div slot="header"></div>
-      <div></div>
+      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
+      <el-table style="width: 100%" :data="attrList">
+        <el-table-column type="index" label="序号" width="80" align="center">
+        </el-table-column>
+        <el-table-column
+          prop="attrName"
+          label="属性名称"
+          width="150"
+          align="center"
+        >
+        </el-table-column>
+        <el-table-column prop="prop" label="属性值列表" width="width">
+          <template slot-scope="{ row, $index }">
+            <el-tag
+              type="success"
+              v-for="(attrValue, index) in row.attrValueList"
+              :key="attrValue.id"
+              style="margin-right: 30px"
+            >
+              {{ attrValue.valueName }}
+            </el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="prop" label="操作" width="150" align="center">
+          <template slot-scope="{ row, $index }">
+            <el-button
+              type="warning"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
@@ -25,11 +62,20 @@ export default {
       category1Id: "",
       category2Id: "",
       category3Id: "",
+      attrList: [],
     };
   },
 
   mounted() {},
   methods: {
+    async getAttrList(x, y, z) {
+      // console.log(x, y, z);
+      // console.log(this);
+      let request = await this.$API.attr.reqAttrList(x, y, z);
+      if (request.code == 200) {
+        this.attrList = request.data;
+      }
+    },
     getCategoryId(data) {
       this.category1Id = data[0];
       this.category2Id = data[1];
@@ -37,6 +83,7 @@ export default {
       console.log(this.category1Id);
       console.log(this.category2Id);
       console.log(this.category3Id);
+      this.getAttrList(this.category1Id, this.category2Id, this.category3Id);
     },
     clear1() {
       this.category2Id = "";
