1bef93fe249015394dab0454759ba327a8128a2a
zhangziheng <zhangziheng_sonder@163.com>
2022-07-19 17:36:52 +0800

    属性管理静态页面

--- vue-admin-template/src/api/product/attr.js
+++ vue-admin-template/src/api/product/attr.js
@@ -8,10 +8,10 @@ export const reqCategory1List = () => request({
 
 ///admin/product/get/category2/{id} 二级
 export const reqCategory2List = (category1Id) => request({
-        url: `/admin/product/getCategory2/${category1Id}`,
-        method: "get"
-    })
-    ///admin/product/get/category3/{id} 三级
+    url: `/admin/product/getCategory2/${category1Id}`,
+    method: "get"
+})
+///admin/product/get/category3/{id} 三级
 export const reqCategory3List = (category2Id) => request({
     url: `/admin/product/getCategory3/${category2Id}`,
     method: "get"
@@ -22,3 +22,24 @@ export const reqAttrList = (category1Id, category2Id, category3Id) => request({
     url: `/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`,
     method: 'get'
 })
+
+// /admin/product/saveAttrInfo 添加属性与属性值
+export const reqAddAttr = (data) => request({
+    url: `/admin/product/saveAttrInfo`,
+    method: 'post',
+    data
+})
+
+//  {
+//     "attrName": "string",
+//     "attrValueList": [
+//       {
+//         "attrId": 0,
+//         "id": 0,
+//         "valueName": "string"
+//       }
+//     ],
+//     "categoryId": 0,
+//     "categoryLevel": 0,
+//     "id": 0
+//   }
--- vue-admin-template/src/views/product/Attr/index.vue
+++ vue-admin-template/src/views/product/Attr/index.vue
@@ -11,44 +11,114 @@
     </el-card>
 
     <el-card>
-      <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
-      <el-table style="width: 100%" :data="attrList">
-        <el-table-column type="index" label="序号" width="80" align="center">
-        </el-table-column>
-        <el-table-column
-          prop="attrName"
-          label="属性名称"
-          width="150"
-          align="center"
+      <div v-show="isShow">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="!category3Id"
+          @click="isShow = false"
+          >添加属性</el-button
         >
-        </el-table-column>
-        <el-table-column prop="prop" label="属性值列表" width="width">
-          <template slot-scope="{ row, $index }">
-            <el-tag
-              type="success"
-              v-for="(attrValue, index) in row.attrValueList"
-              :key="attrValue.id"
-              style="margin-right: 30px"
-            >
-              {{ attrValue.valueName }}
-            </el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="prop" label="操作" width="150" align="center">
-          <template slot-scope="{ row, $index }">
-            <el-button
-              type="warning"
-              icon="el-icon-edit"
-              size="mini"
-            ></el-button>
-            <el-button
-              type="danger"
-              icon="el-icon-delete"
-              size="mini"
-            ></el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+        <el-table style="width: 100%" :data="attrList">
+          <el-table-column type="index" label="序号" width="80" align="center">
+          </el-table-column>
+          <el-table-column
+            prop="attrName"
+            label="属性名称"
+            width="150"
+            align="center"
+          >
+          </el-table-column>
+          <el-table-column prop="prop" label="属性值列表" width="width">
+            <template slot-scope="{ row, $index }">
+              <el-tag
+                type="success"
+                v-for="(attrValue, index) in row.attrValueList"
+                :key="attrValue.id"
+                style="margin-right: 30px"
+              >
+                {{ attrValue.valueName }}
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="prop" label="操作" width="150" align="center">
+            <template slot-scope="{ row, $index }">
+              <el-button
+                type="warning"
+                icon="el-icon-edit"
+                size="mini"
+                @click="isShow = false"
+              ></el-button>
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <!-- 修改属性的结构 -->
+      <div v-show="!isShow">
+        <el-form :inline="true" ref="form" label-width="80px">
+          <el-form-item label="属性名">
+            <el-input
+              placeholder="请输入属性名"
+              v-model="attrInfo.attrName"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="primary" icon="el-icon-plus" @click="addAttrValue"
+          >添加属性值</el-button
+        >
+        <el-button>取消</el-button>
+        <el-table
+          style="width: 100%; margin: 25px 0"
+          border
+          :data="attrInfo.attrValueList"
+        >
+          <el-table-column
+            header-align="center"
+            align="center"
+            type="index"
+            label="序号"
+            width="80"
+          >
+          </el-table-column>
+          <el-table-column
+            width="width"
+            header-align="center"
+            align="center"
+            prop="prop"
+            label="属性值名称"
+          >
+            <template slot-scope="{ row, $index }">
+              <el-input
+                v-model="row.valueName"
+                placeholder="请输入属性值名称"
+                @change="ex(row)"
+              ></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column
+            width="width"
+            header-align="center"
+            align="center"
+            prop="prop"
+            label="操作"
+          >
+            <template slot-scope="{ row, $index }">
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShow = true">取消</el-button>
+      </div>
     </el-card>
   </div>
 </template>
@@ -63,11 +133,23 @@ export default {
       category2Id: "",
       category3Id: "",
       attrList: [],
+      //控制table的显示和隐藏
+      isShow: false,
+      //手机新增属性|修改属性
+      attrInfo: {
+        attrName: "",
+        attrValueList: [],
+        categoryId: 0,
+        categoryLevel: 3,
+      },
     };
   },
 
   mounted() {},
   methods: {
+    ex(row) {
+      console.log(row);
+    },
     async getAttrList(x, y, z) {
       // console.log(x, y, z);
       // console.log(this);
@@ -91,6 +173,13 @@ export default {
     },
     clear2() {
       this.category3Id = "";
+    },
+    addAttrValue() {
+      // 向属性值的数组里面添加元素
+      this.attrInfo.attrValueList.push({
+        attrId: undefined,
+        valueName: "",
+      });
     },
   },
 };
